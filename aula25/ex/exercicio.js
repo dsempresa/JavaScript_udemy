f7972b32d5278ddd16943348478ae4f288a0fd0f
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-16 06:06:43 -0300

    fim do exercício 25

--- aula25/ex/exercicio.js
+++ aula25/ex/exercicio.js
@@ -1,7 +1,11 @@
-let nome = prompt('Digite seu nome:')
-let quantidadeLetras = nome.length
-let quantLetras= quantidadeLetras.split(' ')
-alert(document.body.innerHTML += `Seu nome é: ${nome} <br/ >`)
-alert(document.body.innerHTML += `Seu nome tem ${quantLetras} letras <br>`)
-alert(document.body.innerHTML) += `A segunda letra do seu nome é: <br>`
-alert(document.body.innerHTML) += `Qual primeiro indíce da letra "a" no seu nome?<br>`
+let nome = prompt('Digite seu nome completo:')
+
+document.body.innerHTML = `Seu nome é: ${nome} <br/>`
+document.body.innerHTML += `Seu nome tem ${nome.length} letras <br/>`
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br/>`
+document.body.innerHTML += `Qual primeiro indíce da letra "a" no seu nome? ${nome.indexOf('a')}<br/>`
+document.body.innerHTML += `Qual o ultimo índice da letra "a" no seu nome? ${nome.lastIndexOf('a')}<br/>`
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br/>`
+document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ')}<br/>`
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br/>`
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br/>`
