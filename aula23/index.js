07c5a11240310668240b7dac321193969591b2ce
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-16 03:23:20 -0300

    aulas 24 - String

---
+++ aula23/index.js
@@ -0,0 +1,10 @@
+let varA = 'A'
+let varB = 'B'
+let varC = 'C'
+
+//console.log(varA, varB, varC)
+let variA = varB
+varB = varC
+varC = varA 
+
+console.log(variA, varB, varC)
---
+++ aula24/string.js
@@ -0,0 +1,13 @@
+//               01234567
+let umaString = 'O rato roeu a roupa do rei de roma.'
+
+//console.log(umaString.match(/[A-Z]/)) //expressões regulares
+//console.log(umaString.replace(/r/g , 'R')) // Substitui o valor pelo outro
+//console.log(umaString.length) // saber o tamanho da string
+//console.log(umaString.split(' '))// separando as strings
+//console.log(umaString.toLowerCase()) // Deixando todas as strings com letras minusculas
+//console.log(umaString.toUpperCase()) //Deixando toda a string com letras maiusculas
+
+
+
+
