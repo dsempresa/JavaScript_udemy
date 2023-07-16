f7d1ba4238bb1cdecfcc3bc15b4adbd17ebed273
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-16 06:30:12 -0300

    aula 26

---
+++ aula26/index.js
@@ -0,0 +1,17 @@
+// Transformando numero em string.
+
+
+
+let num1 = 0.7 // Number
+let num2 = 0.1 // Number
+console.log(num1 + num2)
+
+/*console.log(num1.toString() + num2)// Quando há um sinal de '+' após uma string, não será realizado uma operação de adição,mas sim uma concactação entre os valores.
+num1 = num1.toString()
+console.log(typeof num1)
+//console.log(num1.toString(2)) // Vê a representação binária do número
+//console.log(num1.toFixed(2)) //fixando as casas décimais após a vírgula.
+//console.log(Number.isInteger(num1)) // Saber se o número é inteiro.
+let temp = num1 * 'Olá'
+console.log(Number.isNaN(temp))*/
+
