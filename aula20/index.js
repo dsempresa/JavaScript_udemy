d079cb33399f4915dcbd83a777f6274d2a4a11a9
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-16 01:46:12 -0300

    aulas 20 e 21

---
+++ aula20/index.js
@@ -0,0 +1,14 @@
+// NaN = Not a number
+
+// operadores de :
+// INCREMENTO = ++
+// DECREMENTO = --
+
+
+let contador = 1
+++contador
+++contador
+++contador
+++contador
+console.log(contador++)
+console.log(contador)
---
+++ aula21/index.js
@@ -0,0 +1,6 @@
+// methodo = quando está dentro de um objeto
+// função = quando está fora de objeto
+// undefined = Não aponta p lugar nenhum na memória
+// função "confirm" = pode retornar até dois valores => true ou false
+
+prompt('Com a nossa mensagem?')
