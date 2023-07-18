aecc717d2fa38c57b78c83763ac3c0ab9e022ffb
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-18 02:14:56 -0300

    aula38-operadores logicos

---
+++ aula37operadores/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores de comparação
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+== igualdade (valor)*****************
+=== igualdade estrita(valor e tipo)
+!= diferente (valor) ***********
+!== diferente estrito (valor e tipo)
+*/
+
+/*
+Operadores lógicos
+&& -> AND -> E -> TODAS AS EXPRESSÕES PRECISAM SER VERDADEIRAS PARA RETORNAR TRUE
+|| -> OR -> OU -> BASTA QUE UMA EXPRESSÃO RETORNE VERDADEIRA PARA Q RETORNE TRUE,SÓ RETORNA FALSO SE TODAS FOREM FALSE
+! -> NOT -> NÃO ->
+*/
+
