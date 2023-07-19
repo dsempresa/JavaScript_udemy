730b69fff30a7eb0955fb6da47fa1cd8e99eca80
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-19 04:58:38 -0300

    Create exemplo2.js

---
+++ aula42exercicio/exemplo2.js
@@ -0,0 +1,11 @@
+// capturar evento de submit do formulário
+let form = document.querySelector('#formulario')
+
+form.addEventListener('submit' , function(e){
+    e.preventDefault()
+    console.log('Evento prevenido!')
+})
+
+function resultado(msg) {
+    let resultado = document.querySelector('#resultado')
+}
