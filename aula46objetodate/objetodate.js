c7af479bc2b5707fc9420b43ade40d9f827da719
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-23 01:44:58 -0300

    Create objetodate.js

---
+++ aula46objetodate/objetodate.js
@@ -0,0 +1,5 @@
+//const tresHoras = 60 * 60 * 3 * 1000
+//const umDia = 60 * 60 * 24 * 1000
+//const data = new Date(0 + tresHoras + umDia) // 01/01/1970 Timestamp unix ou época unix
+const data = new Date(2019, 3, 20, 15, 14, 27, 500)
+console.log(data.toString())
