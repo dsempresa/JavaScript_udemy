a830a0d4073602e4d239362f6e97d299438a146c
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-08-07 02:57:27 -0300

    aulas 52 e 53

---
+++ aula52for-classico/for.js
@@ -0,0 +1,14 @@
+/*for (let i = 0; i <= 9; i += 3) {
+    console.log(i)
+}*/
+
+/*for (let i = 0; i <= 9; i += 3) {
+    const par = i % 2 === 0 ? 'par' : 'ímpar'
+    console.log(i, par)
+}*/
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Manga', 'Banana', 'Abacate']
+
+for (let i = 0; i < frutas.length; i ++){
+    console.log(frutas[i])
+}
