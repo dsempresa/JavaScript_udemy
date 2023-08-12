9138769caae089cee2100d4560eabdc59f0887ce
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-08-12 03:35:28 -0300

    aulas 60, 61 - exercícios de ldp

---
+++ aula60exldp01/ex01.js
@@ -0,0 +1,43 @@
+/// Escreva uma função que recebe 2 números e retorne o maior deles
+
+/*let a = 9
+let b = 5
+
+function criar(a, b) {
+    if(a > b) {
+        return a
+    }if(b > a) {
+        return b
+    }else{
+        return false
+    }
+}
+
+console.log(criar(a, b))*/
+
+/*let a = 6
+let b = 5
+
+function criar(a, b) {
+    return a > b ? a : b
+}
+
+console.log(criar(a, b))*/
+
+/*let a = 8
+let b = 5
+
+let criar = (a, b) => {
+    return a > b ? a : b
+}
+
+console.log(criar(a, b))*/
+
+
+let a = 1
+let b = 5
+
+let criar = (a, b) => a > b ? a : b
+console.log(criar(a, b))
+
+
---
+++ aula61exldp02/ex02ldp.js
@@ -0,0 +1,12 @@
+// Escreva uma função chamada ePaisagem que recebe dois argumentos,largura e altura de uma imagem(number). Retorne true se a imagem estiver no modo paisagem.
+
+
+let largura = 1
+let altura = 2
+
+let ePaisagem = (largura, altura) => largura > altura
+
+   
+
+
+console.log(ePaisagem(largura, altura))
