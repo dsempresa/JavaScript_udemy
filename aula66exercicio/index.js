1252c3ff5c08ce3643890764dd8892caaaed52e9
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-08-16 01:28:06 -0300

    Update index.js

--- aula66exercicio/index.js
+++ aula66exercicio/index.js
@@ -1,22 +1,72 @@
-let relogio = document.querySelector('.relogio')
-let iniciar = document.querySelector('.iniciar')
-let pausar = document.querySelector('.pausar')
-let zerar = document.querySelector('.zerar')
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
 
-let disparar = (setInterval, 1000)
 
+const relogio = document.querySelector('.relogio')
+/*const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')*/
+let segundos = 0
+let timer
 
-iniciar.addEventListener('click', function(event){
+function iniciaRelogio() {
+     timer = setInterval(function() {
+        segundos++
+        relogio.innerHTML = criaHoraDosSegundos(segundos)
+    }, 1000)
+}
+
+document.addEventListener('click', function(e) {
+    const el = e.target
+
+    if(el.classList.contains('zerar')) {
+        relogio.style.color = 'black'
+        clearInterval(timer)
+        relogio.innerHTML = '00:00:00'
+        segundos = 0 
+    }
+    
+    if(el.classList.contains('iniciar')) {
+        relogio.style.color = 'black'
+        clearInterval(timer)
+        iniciaRelogio()
+    }
+
+    if(el.classList.contains('pausar')) {
+        relogio.style.color = 'red'
+        clearInterval(timer)
+    }
+
+})
+
+/*iniciar.addEventListener('click', function(event){
+    clearInterval(timer)
+    iniciaRelogio()
     relogio.style.color = 'black'
-    relogio.innerHTML = disparar
+
 })
 
 pausar.addEventListener('click', function(event){
     relogio.style.color = 'red'
+    clearInterval(timer)
 })
 
 zerar.addEventListener('click', function(event){
     relogio.style.color = 'black'
-})
+    clearInterval(timer)
+    relogio.innerHTML = '00:00:00'
+    segundos = 0
+})*/
 
+
+
+}
+
+relogio()
 
