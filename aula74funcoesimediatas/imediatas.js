ba0d29c12e04f3111b1cf7d7862b63712e561663
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-08-26 00:48:46 -0300

    aula 75 IIFE

---
+++ aula74funcoesimediatas/imediatas.js
@@ -0,0 +1,16 @@
+// IIFE  -> Imediately Invoked Function Expression
+
+(function (idade, peso, altura) {
+    const sobrenome = 'Miranda'
+    function criaNome(nome) {
+        return nome + ' ' + sobrenome
+    }
+
+    function falaNome() {
+        console.log(criaNome('Luiz'))
+    }
+
+    falaNome()
+    console.log(idade, peso, altura)
+
+})(30, 80, 1.80)
