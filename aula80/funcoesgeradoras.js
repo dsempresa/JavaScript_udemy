d3e5f9063b49d2efadbc620760c224648eecb110
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-09-21 21:24:31 -0300

    aula 80 - funções geradoras

---
+++ aula80/funcoesgeradoras.js
@@ -0,0 +1,31 @@
+/*function* geradora1() {
+    // código qualquer ...
+    yield 'valor 1'
+     // código qualquer ...
+     yield 'valor 2'
+      // código qualquer ...
+    yield 'valor 3'
+}
+
+const g1 = geradora1()
+/*console.log(g1.next().value)
+console.log(g1.next().value)
+console.log(g1.next().value)*/
+/*for (let valor of g1) {
+    console.log(valor)
+}
+*/
+/*/*function* geradora2() {
+    let i = 0
+
+    while(true) {
+        yield i
+        i++
+    }
+}const g2 = geradora2()
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+console.log(g2.next().value)
+*/
