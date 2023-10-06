e9704eeb82d76cc93f00052cf6f192b958cc4b92
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-10-06 02:43:29 -0300

    Create array.js

---
+++ aula81/array.js
@@ -0,0 +1,26 @@
+/*const nomes = ['Eduardo', 'Maria', 'Joana']
+/*const novo = nomes
+novo.pop()
+console.log(nomes[1].length)
+*/
+
+//adcionar no começo do array -> unshift()
+//adcionar no fim do array -> push()
+
+//remover no inicio do array -> shift()
+//remover no final do array -> pop()
+
+//fatia o array -> slice()
+/***************************************/
+//CONVERTENDO STRING EM ARRAY usando o método -> split()
+/*const nome = 'Luiz Otávio Miranda'
+const nomes = nome.split(' ')
+console.log(nomes)*/
+
+//UNINDO A STRING EM ARRAY usando método -> join()
+
+const nomes = ['Luiz', 'Otávio', 'Miranda' ]
+const nome = nomes.join(' ')
+console.log(nome)
+
+
